aa13f3e41424dce08374f23c72a2dde66ae76c80
Wolfgang Teuber <knugie@gmx.net>
2021-09-24 15:00:02 +0200

    Prevent "implicit declaration of function 'pop_cnt_64'" error

--- ext/count_bits_swar/count_bits_swar.c
+++ ext/count_bits_swar/count_bits_swar.c
@@ -10,13 +10,13 @@ void Init_count_bits_swar() {
   rb_define_global_function("count_bits_swar", method_count_bits_swar, 1);
 }
 
-VALUE method_count_bits_swar(VALUE self, VALUE num) {
-  number = NUM2ULL(num);
-  return INT2NUM(pop_cnt_64(number));
-}
-
 int pop_cnt_64(uint64_t i) {
   i = i - ((i >> 1) & 0x5555555555555555);
   i = (i & 0x3333333333333333) + ((i >> 2) & 0x3333333333333333);
   return (((i + (i >> 4)) & 0x0F0F0F0F0F0F0F0F) * 0x0101010101010101) >> 56;
+}
+
+VALUE method_count_bits_swar(VALUE self, VALUE num) {
+  number = NUM2ULL(num);
+  return INT2NUM(pop_cnt_64(number));
 }
